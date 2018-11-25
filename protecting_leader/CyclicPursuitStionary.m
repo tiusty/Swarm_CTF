1e1c3c30464cb8e5ff9c4d805a3d19bd9dc3b3d6
Tiusty <awagud12@gmail.com>
2018-11-25 04:57:12 -0500

    Got Sids code working with my script and encapsolated the code into a function

--- protecting_leader/CyclicPursuitStionary.m
+++ protecting_leader/CyclicPursuitStionary.m
@@ -15,55 +15,10 @@ x = xuni(1:2,:);                                            % x-y positions only
 r.set_velocities(1:N, zeros(2,N));                       % Assign dummy zero velocity
 r.step();                                                % Run robotarium step
 
-formation_control_circle(N, r , si_to_uni_dyn)
+formation_control_circle(N, r, si_to_uni_dyn)
 
-% Cyclic graph
-% A = diag(ones(N-1,1),-1);
-% A(1,N) = 1; 
-% A(N,1)
-% L = diag(sum(A)) - A;
-L = [ 1 0 -1 0;
-    -1 1 0 0;
-    0 -1 1 0;
-    0 0 0 0]
-
-% Target cycle definition
-center = [0;0];
-radius = 0.7;
-interAgentDistance = radius*2*sin(pi/N);
-kp1 = 7;
-kp2 = 0.4;
-plot(center(1),center(2),'*','markersize',12)
-th = 0 : 2*pi/20 : 2*pi-2*pi/20;
-plot(radius.*cos(th)+center(1),radius.*sin(th)+center(2),'b')
-  
-for k = 1:max_iter
-    
-    % Get new data and initialize new null velocities
-    xuni = r.get_poses();                                % Get new robots' states
-    x = xuni(1:2,:);                                        % Extract single integrator states
-
-    dx = zeros(2,N);                                           % Initialize velocities to zero         
-    for i = 1:N-1                
-         for j = topological_neighbors(L,i)
-            if ~isempty(j)
-                alpha = pi/N + kp1*(interAgentDistance - norm(x(:,j)-x(:,i)) );
-                R = [cos(alpha), sin(alpha); -sin(alpha) cos(alpha)];
-                dx(:,i) = dx(:,i) + R*( x(:,j)-x(:,i) ) - kp2*( x(:,i) - center );
-            end
-        end
-    end
-    
-
-    dx = si_to_uni_dyn(dx, xuni);                            % Convert single integrator inputs into unicycle inputs
-    
-    % Set velocities of agents 1,...,N
-    r.set_velocities(1:N, dx);
-
-    % Send the previously set velocities to the agents.  This function must be called!
-    r.step();    
-    
-end
+radius = .5;
+cyclicPursuit(r, N, radius, max_iter)
 
 % Though we didn't save any data, we still should call r.call_at_scripts_end() after our
 % experiment is over!
---
+++ protecting_leader/cyclicPursuit.m
@@ -0,0 +1,42 @@
+
+function [] = cyclicPursuit(r, N, radius, max_iter)
+
+    circularAgents = N-1;
+    [si_to_uni_dyn] = create_si_to_uni_mapping3();
+    
+    % Cyclic graph
+    L = directedCircleGraphLaplacian(circularAgents);
+    L = [L zeros(circularAgents,2)];
+    L = [L ; zeros(2,N+1)];
+    L(N,N) = 1;
+    L(N,N+1)= -1;
+    % Target cycle definition
+    interAgentDistance = radius*2*sin(pi/circularAgents);
+    kp1 = 7;
+    kp2 = 0.4;
+    xanchor = [-0.7;0.3];
+    for k = 1:max_iter   
+        
+        % Get new data and initialize new null velocities
+        xuni = r.get_poses();                                % Get new robots' states
+        x = xuni(1:2,:);                                        % Extract single integrator states
+        center = x(:,N);
+        dx = zeros(2,N);                                           % Initialize velocities to zero         
+        for i = 1:N               
+            for j = topological_neighbors(L,i)
+                if ~isempty(j)
+                    if(i~= N)
+                        alpha = pi/circularAgents + kp1*(interAgentDistance - norm(x(:,j)-x(:,i)) );
+                        R = [cos(alpha), sin(alpha); -sin(alpha) cos(alpha)];
+                        dx(:,i) = dx(:,i) + R*( x(:,j)-x(:,i) ) - kp2*(x(:,i) - center) + (norm(x(:, i) - center)^2 - radius^2)*(center - x(:, i));
+                    else
+                        dx(:,i) = .01*(xanchor-x(:,i));
+                    end
+                end
+            end
+        end
+        dx = si_to_uni_dyn(dx, xuni);                            % Convert single integrator inputs into unicycle inputs
+        r.set_velocities(1:N, dx); 
+        r.step();
+    end
+end
